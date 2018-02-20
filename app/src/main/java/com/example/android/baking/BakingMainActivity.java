9f984c53778a02b2910cae44b65dac13cc1fdfa5
zuoqi <774765813@qq.com>
2018-02-20 22:57:11 +0800

    Add new main Activity for main UI

---
+++ app/src/main/java/com/example/android/baking/BakingMainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.baking;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by milkdz on 2018/2/20.
+ */
+
+public class BakingMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.baking_main_activity);
+    }
+}
