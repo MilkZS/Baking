2f7b4799f847b7929ef8e6d4a6de89c737b05af0
zuoqi <774765813@qq.com>
2018-03-01 22:58:48 +0800

    Add RecipeDBHelper

---
+++ app/src/main/java/com/example/android/baking/db/RecipeDBHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.baking.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by milkdz on 2018/2/23.
+ */
+
+public class RecipeDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Recipe.db";
+    private static final int VERSION = 1;
+
+    public RecipeDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable());
+    }
+
+    private String createTable(){
+        return SQLBaseInfo.CREATE_TABLE + RecipeContract.RecipeInfo.TABLE_NAME + " ( "
+                + RecipeContract.RecipeInfo._ID + SQLBaseInfo.INT_PRIMARY_KEY + ","
+                + RecipeContract.RecipeInfo.COLUMN_ID + ","
+                + RecipeContract.RecipeInfo.COLUMN_NAME + ","
+                + RecipeContract.RecipeInfo.COLUMN_STEP +  ","
+                + RecipeContract.RecipeInfo.COLUMN_SERVINGS  + ","
+                + RecipeContract.RecipeInfo.COLUMN_INGREDIENTS
+                + " ); ";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQLBaseInfo.DROP_TABLE + RecipeContract.RecipeInfo.TABLE_NAME);
+        onCreate(db);
+    }
+}
