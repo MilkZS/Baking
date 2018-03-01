7d7cc2ede47c228e160e84881b842208c0beeb14
zuoqi <774765813@qq.com>
2018-03-01 22:58:05 +0800

    Add baseinfo

---
+++ app/src/main/java/com/example/android/baking/base/BaseInfo.java
@@ -0,0 +1,32 @@
+package com.example.android.baking.base;
+
+/**
+ * Created by milkdz on 2018/2/22.
+ */
+
+public interface BaseInfo {
+    String REMOTE_HTTP = "https://s3.cn-north-1.amazonaws.com.cn/"
+            + "static-documents/nd801/ProjectResources/Baking/baking-cn.json";
+
+    String RECIPE_ID = "id";
+
+    String RECIPE_NAME = "name";
+
+    String RECIPE_INGREDIENTS = "ingredients";
+    String RECIPE_INGREDIENTS_QUANTITY = "quantity";
+    String RECIPE_INGREDIENTS_MEASURE = "measure";
+    String RECIPE_INGREDIENTS_INGREDIENT = "ingredient";
+
+    String RECIPE_STEP = "steps";
+    String RECIPE_STEP_ID = "id";
+    String RECIPE_STEP_SHORT_DESCRIPTION = "shortDescription";
+    String RECIPE_STEP_DESCRIPTION = "description";
+    String RECIPE_STEP_VIDEO = "videoURL";
+    String RECIPE_STEP_THUMBNAIL = "thumbnailURL";
+
+    String RECIPE_SERVINGS = "servings";
+
+    //chose mode to dis step or ingredient
+    int RECIPE_MODE_INGREDIENTS = 1;
+    int RECIPE_MODE_STEP = 2;
+}
