4c69ebd549927361cf22110e546ba095b5ed238a
zuoqi <774765813@qq.com>
2018-03-04 00:17:55 +0800

    Update anaylize json

--- app/src/main/java/com/example/android/baking/util/ReadFromJsonString.java
+++ app/src/main/java/com/example/android/baking/util/ReadFromJsonString.java
@@ -20,6 +20,9 @@ public class ReadFromJsonString {
     private static String TAG = "ReadFromJsonString";
     private static boolean DBG = true;
 
+    public static String DIVIDE_TYPE = "-";
+    public static String DIVIDE_CONTENT = "#";
+
     private static String QUERY_INGREDIENTS = "ingredients";
 
     public static ContentValues[] buildContentValuesForJson(String jsonString) {
@@ -41,11 +44,10 @@ public class ReadFromJsonString {
                 contentValues.put(RecipeContract.RecipeInfo.COLUMN_SERVINGS,
                         jsonObject.getString(BaseInfo.RECIPE_SERVINGS));
                 contentValues.put(RecipeContract.RecipeInfo.COLUMN_INGREDIENTS,
-                        queryIngredients(jsonObject.getString(BaseInfo.RECIPE_INGREDIENTS),
-                                BaseInfo.RECIPE_MODE_INGREDIENTS));
-                contentValues.put(RecipeContract.RecipeInfo.COLUMN_STEP,
-                        queryIngredients(jsonObject.getString(BaseInfo.RECIPE_STEP),
-                                BaseInfo.RECIPE_MODE_STEP));
+                        queryInfo(jsonObject, RecipeContract.QUERY_INGREDIENTS));
+                //contentValues.put(RecipeContract.RecipeInfo.COLUMN_STEP,
+                 //       queryInfo(jsonObject.getString(BaseInfo.RECIPE_STEP),
+                   //             BaseInfo.RECIPE_MODE_STEP));
                 contentValuesArray[i] = contentValues;
             }
             return contentValuesArray;
@@ -55,31 +57,40 @@ public class ReadFromJsonString {
         return null;
     }
 
-    public static String queryIngredients(String jsonString,String[] QUERY_ARRAY) {
+    public static String queryInfo(JSONObject jsonObject,String[] QUERY_ARRAY) {
 
+       // Log.d(TAG,"query info jsonString is " + jsonString);
         try {
-            JSONObject jsonObjectJson = new JSONObject(jsonString);
-            String s = QUERY_ARRAY[0];
-            for (int i = 1; i < QUERY_ARRAY.length; i++) {
-                s = s + "###" + jsonObjectJson.getString(QUERY_ARRAY[i]);
+            JSONArray jsonArray = jsonObject.getJSONArray(BaseInfo.RECIPE_INGREDIENTS);//new JSONArray(jsonString);
+            String sRe = "";
+            for(int i=0;i<jsonArray.length();i++){
+                JSONObject sObject = jsonArray.getJSONObject(i);
+                sRe = sRe + sObject.getString(QUERY_ARRAY[0]);
+                for (int j=1;j<QUERY_ARRAY.length;j++){
+                    sRe = sRe + DIVIDE_CONTENT + sObject.getString(QUERY_ARRAY[j]);
+                }
+                if (i > 0){
+                    sRe = sRe + DIVIDE_TYPE ;
+                }
             }
-            return s;
+            Log.d(TAG,"query info string array is " + sRe);
+            return sRe;
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
     }
-
-    public static String queryIngredients(String jsonString, int mode) {
+/*
+    public static String queryInfo(String jsonString, int mode) {
         switch (mode) {
             case BaseInfo.RECIPE_MODE_INGREDIENTS: {
-               return queryIngredients(jsonString,RecipeContract.QUERY_INGREDIENTS);
+               return queryInfo(jsonString,RecipeContract.QUERY_INGREDIENTS);
             }
             case BaseInfo.RECIPE_MODE_STEP: {
-                return queryIngredients(jsonString,RecipeContract.QUERY_STEP);
+                return queryInfo(jsonString,RecipeContract.QUERY_STEP);
             }
         }
         return null;
     }
-
+*/
 }
