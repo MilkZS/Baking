d79770ec98a23422a5d9363d07dc15953cf3fdbe
zuoqi <774765813@qq.com>
2018-03-04 22:40:55 +0800

    update Form

--- app/src/main/java/com/example/android/baking/util/FormRecipe.java
+++ app/src/main/java/com/example/android/baking/util/FormRecipe.java
@@ -3,7 +3,11 @@ package com.example.android.baking.util;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.example.android.baking.base.RecipeStep;
+import com.example.android.baking.base.RecipeSteps;
 import com.example.android.baking.db.RecipeContract;
+
+import java.util.ArrayList;
 
 /**
  * Created by milkdz on 2018/3/4.
@@ -13,21 +17,45 @@ public class FormRecipe {
 
     private static String TAG = "FormRecipe";
 
-    public static String formIngredients(String sArray){
+    public static String formIngredients(String sArray) {
 
-        Log.d(TAG,"input array string is " + sArray);
+        Log.d(TAG, "input Ingredients array string is " + sArray);
         String[] s = sArray.split(ReadFromJsonString.DIVIDE_TYPE);
         String showData = "\t\t\t\t";
-        for (int i=0;i<s.length;i++) {
+        for (int i = 0; i < s.length; i++) {
             String[] ss = s[i].split(ReadFromJsonString.DIVIDE_CONTENT);
-            Log.d(TAG,"ss[i] content is " + s[i]);
-            Log.d(TAG,"ss length is " + ss.length);
-            if(i != s.length - 1){
+            Log.d(TAG, "ss[i] content is " + s[i]);
+            Log.d(TAG, "ss length is " + ss.length);
+            if (i != s.length - 1) {
                 showData = showData + ss[0] + "\t" + ss[1] + "\t" + ss[2] + ",\t";
-            }else{
+            } else {
                 showData = showData + ss[0] + "\t" + ss[1] + "\t" + ss[2] + ".\t\n";
             }
         }
         return showData;
+    }
+
+    public static RecipeSteps addArrayList(String sArray) {
+        Log.d(TAG, "input steps array string is " + sArray);
+        RecipeSteps recipeSteps = new RecipeSteps();
+        ArrayList<RecipeStep> recipeStepArrayList = new ArrayList<>();
+        String[] s = sArray.split(ReadFromJsonString.DIVIDE_TYPE);
+        for (int i = 0; i < s.length; i++) {
+            String[] ss = s[i].split(ReadFromJsonString.DIVIDE_CONTENT);
+            Log.d(TAG, "ss[i] content is " + s[i]);
+            Log.d(TAG, "ss length is " + ss.length);
+            RecipeStep recipeStep = new RecipeStep();
+            recipeStep.setId(ss[0]);
+            recipeStep.setsTitle(ss[1]);
+            recipeStep.setsDescription(ss[2]);
+            if(!ss[3].equals("")){
+                recipeStep.setsVideo(ss[3]);
+            }else if (!ss[4].equals("")){
+                recipeStep.setsVideo(ss[4]);
+            }
+            recipeStepArrayList.add(recipeStep);
+        }
+        recipeSteps.setRecipeStepArrayList(recipeStepArrayList);
+        return recipeSteps;
     }
 }
