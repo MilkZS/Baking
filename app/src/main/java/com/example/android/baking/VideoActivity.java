c961717439d466e57d80293d26b90019ab83a227
qi.x.zuo <qi.x.zuo@sony.com>
2018-03-06 16:29:37 +0800

    Add VideoActivity

---
+++ app/src/main/java/com/example/android/baking/VideoActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.baking;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by milkz on 18-3-6.
+ */
+
+public class VideoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
