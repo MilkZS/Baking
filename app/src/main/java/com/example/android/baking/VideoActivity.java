a8f1c6b2d688450b6f28aa63ecb81be1fbac1f02
qi.x.zuo <qi.x.zuo@sony.com>
2018-03-07 19:59:40 +0800

    Update VideoActivity

--- app/src/main/java/com/example/android/baking/VideoActivity.java
+++ app/src/main/java/com/example/android/baking/VideoActivity.java
@@ -1,18 +1,28 @@
 package com.example.android.baking;
 
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.baking.base.BaseInfo;
+import com.example.android.baking.base.RecipeStep;
+import com.example.android.baking.base.RecipeSteps;
 
 /**
  * Created by milkz on 18-3-6.
  */
 
 public class VideoActivity extends AppCompatActivity {
+
+    private int position;
+    private RecipeSteps recipeSteps ;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Intent intent = getIntent();
+        position = intent.getIntExtra(BaseInfo.INTENT_LIST_INDEX,0);
     }
 }
