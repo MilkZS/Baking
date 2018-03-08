d37b7759a83d8dcf185c65f349d17ea2033bac75
zuoqi <774765813@qq.com>
2018-03-08 22:54:22 +0800

    update VideoActivity

--- app/src/main/java/com/example/android/baking/VideoActivity.java
+++ app/src/main/java/com/example/android/baking/VideoActivity.java
@@ -1,40 +1,78 @@
 package com.example.android.baking;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.android.baking.base.BaseInfo;
+import com.example.android.baking.base.RecipeStep;
 import com.example.android.baking.base.RecipeSteps;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+import java.util.ArrayList;
 
 /**
  * Created by milkz on 18-3-6.
  */
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity implements ExoPlayer.EventListener{
 
     private int position;
-    private RecipeSteps recipeSteps ;
+    private RecipeStep recipeStep;
+    private RecipeSteps recipeSteps;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Toast.makeText(this,"this is oncreate",Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        position = intent.getIntExtra(BaseInfo.INTENT_LIST_INDEX,0);
-        recipeSteps = (RecipeSteps) intent.getSerializableExtra(BaseInfo.INTENT_LIST_INDEX);
+        if(intent.hasExtra(BaseInfo.INTENT_RECIPE)){
+            recipeSteps = (RecipeSteps) intent.getSerializableExtra(BaseInfo.INTENT_RECIPE);
 
+        }
+        if (intent.hasExtra(BaseInfo.INTENT_LIST_INDEX)){
+            position = intent.getIntExtra(BaseInfo.INTENT_LIST_INDEX,0);
+        }
+
+        recipeStep = recipeSteps.getRecipeStepArrayList().get(position);
+        String urlS = recipeStep.getsVideo();
+        Uri uri = Uri.parse(urlS);
+
+        mPlayerView = findViewById(R.id.video_step);
+
+
+
+        initializePlayer(uri);
     }
 
+    /*
     private void videoImpl(){
         Handler videoHandler = new Handler();
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
@@ -43,16 +81,72 @@ public class VideoActivity extends AppCompatActivity {
         TrackSelector trackSelector =
                 new DefaultTrackSelector(videoTrackSelectionFactory);
         // 2. Create the player
-        //SimpleExoPlayer player =
-              //  ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        SimpleExoPlayer player =
+               ExoPlayerFactory.newSimpleInstance(this, trackSelector);
 
 
+    }*/
+
+
+    /**
+     * Initialize ExoPlayer.
+     * @param mediaUri The URI of the sample to play.
+     */
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            // Set the ExoPlayer.EventListener to this activity.
+            mExoPlayer.addListener(this);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(this, "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    this, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
 
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
 
     }
 }
