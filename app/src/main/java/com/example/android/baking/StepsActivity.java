9db9083d8aad17214e0c113a4d82f32f2c485182
zuoqi <774765813@qq.com>
2018-03-11 20:25:28 +0800

    update StepsActivity

--- app/src/main/java/com/example/android/baking/StepsActivity.java
+++ app/src/main/java/com/example/android/baking/StepsActivity.java
@@ -1,72 +1,169 @@
 package com.example.android.baking;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.baking.adapter.StepRecycleAdapter;
 import com.example.android.baking.base.BaseInfo;
 import com.example.android.baking.base.RecipeStep;
 import com.example.android.baking.base.RecipeSteps;
+import com.example.android.baking.base.TakeValues;
+import com.example.android.baking.fragment.StepFragment;
+import com.example.android.baking.fragment.VideoFragment;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 
 import java.util.ArrayList;
+import java.util.EventListener;
 
-public class StepsActivity extends AppCompatActivity implements StepRecycleAdapter.VideoClick{
+public class StepsActivity extends AppCompatActivity implements StepRecycleAdapter.VideoClick, ExoPlayer.EventListener,StepFragment.ChangePosition {
 
     private String TAG = "StepsActivity";
     private ArrayList<RecipeStep> recipeStepArrayList;
     private RecipeSteps recipeSteps;
-    private String label ;
+    private String label;
     private TextView prepareText;
+    private FragmentTransaction FT;
+    private StepFragment stepFragment;
+    private VideoFragment videoFragment;
+    private boolean ifUseFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_steps);
 
-        RecyclerView recyclerView = findViewById(R.id.recipe_step_recycle);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);
-
-        prepareText = findViewById(R.id.prepare_text_view);
+        ifUseFragment = TakeValues.ifUseFragment;
+        if (ifUseFragment) {
+            setContentView(R.layout.activity_step_video_pad);
+        } else {
+            setContentView(R.layout.activity_steps);
+            prepareText = findViewById(R.id.prepare_text_view);
+        }
 
         Intent intent = getIntent();
 
-        if(intent.hasExtra(BaseInfo.INTENT_PREPARE)){
+        if (intent.hasExtra(BaseInfo.INTENT_PREPARE)) {
             String s = intent.getStringExtra(BaseInfo.INTENT_PREPARE);
-            prepareText.setText(s);
+            if (!ifUseFragment) {
+                prepareText.setText(s);
+            }
+            TakeValues.prepareText = s;
         }
 
-        if(intent.hasExtra(BaseInfo.INTENT_TITLE)){
-            label = "Recipe " + intent.getStringExtra(BaseInfo.INTENT_TITLE);
+        TakeValues.eventListener = this;
+
+        if (intent.hasExtra(BaseInfo.INTENT_TITLE)) {
+            label = intent.getStringExtra(BaseInfo.INTENT_TITLE);
             setTitle(label);
-        }else{
-            label = "Recipe video";
+        } else {
+            label = "video";
         }
-        if(intent.hasExtra(BaseInfo.INTENT_LIST)) {
+        if (intent.hasExtra(BaseInfo.INTENT_LIST)) {
 
             recipeSteps = (RecipeSteps) intent.getSerializableExtra(BaseInfo.INTENT_LIST);
             recipeStepArrayList = recipeSteps.getRecipeStepArrayList();
-            StepRecycleAdapter stepRecycleAdapter = new StepRecycleAdapter(recipeStepArrayList, this);
-
-            recyclerView.setAdapter(stepRecycleAdapter);
+           // TakeValues.recipeStepsArrayList = recipeStepArrayList;
+            if (!ifUseFragment) {
+                RecyclerView recyclerView = findViewById(R.id.recipe_step_recycle);
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setHasFixedSize(true);
+                StepRecycleAdapter stepRecycleAdapter = new StepRecycleAdapter(recipeStepArrayList, this);
+                recyclerView.setAdapter(stepRecycleAdapter);
+            }
         }
-    }
 
+        if (ifUseFragment && savedInstanceState == null) {
+            //if(stepFragment == null){
+                stepFragment = new StepFragment();
+                stepFragment.setRecipeStepsArrayList(recipeStepArrayList);
+                stepFragment.setChangePosition(this);
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_step, stepFragment).commit();
+
+
+            //}
+
+//            if(videoFragment == null){
+            Log.e(TAG, "it is going to create VideoFragment");
+            videoFragment = new VideoFragment();
+            videoFragment.setRecipeStepArrayList(recipeStepArrayList);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_video, videoFragment).commit();
+            //          }
+        }
+
+    }
 
 
     @Override
     public void onClick(int position) {
-        Log.d(TAG,"it is click ");
-        Intent intent = new Intent(this, VideoActivity.class);
-        intent.putExtra(BaseInfo.INTENT_RECIPE,recipeSteps);
-        intent.putExtra(BaseInfo.INTENT_LIST_INDEX,position);
-        intent.putExtra(BaseInfo.INTENT_TITLE,label);
-        this.startActivity(intent);
+        Log.d(TAG, "it is click ");
+
+        if (!ifUseFragment) {
+            Intent intent = new Intent(this, VideoActivity.class);
+            intent.putExtra(BaseInfo.INTENT_RECIPE, recipeSteps);
+            intent.putExtra(BaseInfo.INTENT_LIST_INDEX, position);
+            intent.putExtra(BaseInfo.INTENT_TITLE, label);
+            this.startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void change(int position) {
+        VideoFragment videoFragment = new VideoFragment();
+        videoFragment.setRecipeStepArrayList(recipeStepArrayList);
+        videoFragment.setPosition(position);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_video,videoFragment).commit();
     }
 }
