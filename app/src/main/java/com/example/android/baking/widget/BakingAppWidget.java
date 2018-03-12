f000dce56f454c377dca18b8e3d003a7d8a447ed
zuoqi <774765813@qq.com>
2018-03-12 23:26:32 +0800

    Add widget

--- app/src/main/java/com/example/android/baking/widget/BakingAppWidget.java
+++ app/src/main/java/com/example/android/baking/widget/BakingAppWidget.java
@@ -18,38 +18,6 @@ import com.example.android.baking.base.TakeValues;
  */
 public class BakingAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        String sName = TakeValues.label;//TakeValues.recipeStepsArrayList.get(TakeValues.widgetPosition).getsTitle();
-        Log.d("widget ","widget name is ==> " + sName);
-
-        String sParpre = "";
-
-        String[] s = TakeValues.prepareText.split(",");
-        for(int i=0;i<s.length-1;i++){
-            sParpre = sParpre + s[i] + "," + "\n";
-        }
-        sParpre = sParpre + s[s.length - 1] + ".";
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_app_widget);
-        //views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        views.setTextViewText(R.id.name_widget,sName);
-        views.setTextViewText(R.id.show_widget,sParpre);
-
-        Intent intent = new Intent(context,StepsActivity.class);
-        intent.putExtra(BaseInfo.INTENT_LIST,TakeValues.recipeSteps);
-        intent.putExtra(BaseInfo.INTENT_TITLE,sName);
-        intent.putExtra(BaseInfo.INTENT_PREPARE,TakeValues.prepareText);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
-        views.setOnClickPendingIntent(R.id.name_widget,pendingIntent);
-        views.setOnClickPendingIntent(R.id.show_widget,pendingIntent);
-
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -61,7 +29,7 @@ public class BakingAppWidget extends AppWidgetProvider {
             );
             Intent intent = new Intent(context, WidgetService.class);
             views.setRemoteAdapter(R.id.widget_list, intent);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            appWidgetManager.updateAppWidget(appWidgetIds, views);
 
         }
     }
