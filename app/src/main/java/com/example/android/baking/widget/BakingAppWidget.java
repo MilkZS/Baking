97b98b253afac77a8d662395e6fb73b4dee65d4d
qi.x.zuo <qi.x.zuo@sony.com>
2018-03-13 20:01:30 +0800

    Update BakingAppWidget.java

--- app/src/main/java/com/example/android/baking/widget/BakingAppWidget.java
+++ app/src/main/java/com/example/android/baking/widget/BakingAppWidget.java
@@ -3,6 +3,7 @@ package com.example.android.baking.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -20,6 +21,7 @@ import com.example.android.baking.base.TakeValues;
  */
 public class BakingAppWidget extends AppWidgetProvider {
 
+    public static final String BUTTON_ACTION = "com.example.android.baking.click";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -33,8 +35,21 @@ public class BakingAppWidget extends AppWidgetProvider {
            // views.setPendingIntentTemplate();
             SharedPreferences sharedPreferences = context.getSharedPreferences(BaseInfo.PREFERENCE_WIDGET,Context.MODE_PRIVATE);
             int po = sharedPreferences.getInt(BaseInfo.PREFERENCE_WIDGET_POSITION,0);
-            views.setTextViewText(R.id.name_widget,TakeValues.widgetArr.get(po).get(0));
+            String name = TakeValues.widgetArr.get(po).get(0);
+            views.setTextViewText(R.id.name_widget,name);
             views.setRemoteAdapter(R.id.prepare_list_view_widget, intent);
+
+            Intent intentName = new Intent(context,StepsActivity.class);
+            intentName.putExtra(BaseInfo.INTENT_TITLE,name);
+            intentName.putExtra(BaseInfo.INTENT_LIST,TakeValues.recipeArr.get(po));
+            intentName.putExtra(BaseInfo.INTENT_PREPARE,TakeValues.widgetArr.get(po).get(2));
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intentName,0);
+            views.setOnClickPendingIntent(R.id.name_widget,pendingIntent);
+
+           // Intent intentBut = new Intent(BUTTON_ACTION);
+            //PendingIntent pendingIntentBut = PendingIntent.getBroadcast(context, R.id.change_but, intentBut, PendingIntent.FLAG_UPDATE_CURRENT);
+            //views.setOnClickPendingIntent(R.id.change_but,pendingIntentBut);
+
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
@@ -47,6 +62,18 @@ public class BakingAppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+
+    public static void sendRefreshBroadcast(Context context) {
+        Intent intent = new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        intent.setComponent(new ComponentName(context, BakingAppWidget.class));
+        context.sendBroadcast(intent);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
     }
 }
 
