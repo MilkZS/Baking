469277f9adae33488df3d219d0c97e2a3ccd1bca
qi.x.zuo <qi.x.zuo@sony.com>
2018-03-15 19:32:40 +0800

    Update NetWorkUtil.java

--- app/src/main/java/com/example/android/baking/util/NetWorkUtil.java
+++ app/src/main/java/com/example/android/baking/util/NetWorkUtil.java
@@ -1,13 +1,11 @@
 package com.example.android.baking.util;
 
-import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -20,23 +18,6 @@ public class NetWorkUtil {
     private static String TAG = "NetWorkUtil";
 
     /**
-     * Build url with http string address
-     *
-     * @param sHttpAdd address
-     * @return url
-     * @throws MalformedURLException
-     */
-    @org.jetbrains.annotations.Contract("null -> null")
-    public static URL buildUrlFromHttp(String sHttpAdd) throws MalformedURLException {
-        if(sHttpAdd == null){
-            return null;
-        }
-        Uri uri = Uri.parse(sHttpAdd).buildUpon().build();
-        URL url = new URL(uri.toString());
-        return url;
-    }
-
-    /**
      * This method returns the entire result from the HTTP response.
      *
      * @param url The URL to fetch the HTTP response from.
@@ -47,7 +28,6 @@ public class NetWorkUtil {
     public static String getResponseFromHttpUrl(URL url) throws IOException {
         Log.d(TAG,"url -- > " + url);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        //urlConnection.setConnectTimeout(10000);
         try {
             InputStream in = urlConnection.getInputStream();
 
@@ -64,5 +44,4 @@ public class NetWorkUtil {
             urlConnection.disconnect();
         }
     }
-
 }
