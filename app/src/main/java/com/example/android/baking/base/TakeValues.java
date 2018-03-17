05340a359ad8a6d1eac61ebae9dc5f96cb0cc452
zuoqi <774765813@qq.com>
2018-03-17 11:35:24 +0800

    Update TakeValues

--- app/src/main/java/com/example/android/baking/base/TakeValues.java
+++ app/src/main/java/com/example/android/baking/base/TakeValues.java
@@ -2,6 +2,8 @@ package com.example.android.baking.base;
 
 import com.google.android.exoplayer2.ExoPlayer;
 
+import java.util.ArrayList;
+
 /**
  * Created by milkdz on 2018/3/10.
  */
@@ -13,8 +15,10 @@ public class TakeValues {
     /**  values for step */
     public static String prepareText = "";
     public static String label = "";
+    public static ArrayList<RecipeStep> recipeStepArrayList = new ArrayList<>();
 
     /** values for video */
     public static ExoPlayer.EventListener eventListener;
     public static long videoPosition = 0;
+    public static boolean ifChange = false;
 }
